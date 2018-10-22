ae319a96604cc3f38a1432e7fc1c975b8b6680f4
archnivs <archnivs@gmail.com>
2018-10-22 08:20:57 +0800

    adding id attribute to main layout tags

--- index.js
+++ index.js
@@ -4,12 +4,8 @@ const toggleSidebar = document.getElementById("toggle-sidebar")
 
 toggleSidebar.addEventListener(
     "click",
-    (e) => {
-        aside.setAttribute(
-            'data-state', 
-            aside.getAttribute('data-state') === 'open' ? 'closed' : 'open'
-        )
-    }
+    () => aside
+            .setAttribute('data-state', aside.getAttribute('data-state') === 'open' ? 'closed' : 'open')
 )
 
 
